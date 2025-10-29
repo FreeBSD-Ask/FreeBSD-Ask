67c7e93f50318bf5131d6bfddd8a69fbbc85ac8a
ykla <yklaxds@gmail.com>
2025-10-29 21:40:52 +0800

    测试字重 (#1399)
    * 测试字重
    
    * Update index.js
    
    * Update custom.css
    
    * Update custom.css

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -3,7 +3,6 @@
 import DefaultTheme from 'vitepress/theme-without-fonts'; //不使用默认字体
 import './custom.css';
 import Layout from './Layout.vue';
-import 'lxgw-wenkai-screen-web/style.css';
 
 
 export default {
