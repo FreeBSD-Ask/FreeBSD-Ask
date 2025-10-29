fd5c526aa151f26133a8a1eccf82e08e133b7321
ykla <yklaxds@gmail.com>
2025-10-29 20:03:07 +0800

    本地化字体 (#1394)
    * 更新 custom.css
    
    * 更新 index.js
    
    * 更新 index.js

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -3,6 +3,8 @@
 import DefaultTheme from 'vitepress/theme';
 import './custom.css';
 import Layout from './Layout.vue';
+import 'lxgw-wenkai-gb-web/style.css;
+
 
 export default {
   ...DefaultTheme,
