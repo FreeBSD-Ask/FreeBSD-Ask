1c32ef901f2edcc7c96f4df8fc7a6486c14f028b
ykla <yklaxds@gmail.com>
2025-10-29 20:30:55 +0800

    不引入默认字体

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -3,6 +3,7 @@
 import DefaultTheme from 'vitepress/theme';
 import './custom.css';
 import Layout from './Layout.vue';
+import DefaultTheme from 'vitepress/theme-without-fonts'; //不使用默认字体
 import 'lxgw-wenkai-gb-web/style.css';
 
 
