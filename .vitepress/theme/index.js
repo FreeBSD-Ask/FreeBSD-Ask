91c5b5a183a542e4289660354ec58b6588d8250f
ykla <yklaxds@gmail.com>
2025-10-30 02:02:31 +0800

    更新 index.js

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -2,7 +2,7 @@
 
 import DefaultTheme from 'vitepress/theme-without-fonts'; //不使用默认字体
 import 'lxgw-wenkai-screen-web/style.css'; //导入霞鹜文楷屏幕阅读版
-import 'noto-sans-sc/all.css' // 导入 noto
+import 'noto-sans-sc/all.css'; // 导入 noto
 import './custom.css';
 import Layout from './Layout.vue';
 
