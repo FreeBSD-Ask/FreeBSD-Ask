169f997283a697cc101e319e6fd28f147f64e1b4
ykla <yklaxds@gmail.com>
2025-10-29 21:38:10 +0800

    Update index.js

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -3,7 +3,6 @@
 import DefaultTheme from 'vitepress/theme-without-fonts'; //不使用默认字体
 import './custom.css';
 import Layout from './Layout.vue';
-import 'lxgw-wenkai-gb-web/style.css';
 
 
 export default {
