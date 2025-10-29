addc1cf654bae4b47956501135c56b12c2afb473
ykla <yklaxds@gmail.com>
2025-10-29 19:56:50 +0800

    更新 index.js

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -3,6 +3,9 @@
 import DefaultTheme from 'vitepress/theme';
 import './custom.css';
 import Layout from './Layout.vue';
+import 'lxgw-wenkai-gb-web/style.css;
+import 'lxgwwenkaimonogb-regular/style.css';
+
 
 export default {
   ...DefaultTheme,
