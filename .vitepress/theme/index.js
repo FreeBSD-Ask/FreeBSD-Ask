28d470ff89f496ae56fecbf087cc1488f4fa93d6
ykla <yklaxds@gmail.com>
2025-10-30 12:07:59 +0800

    导入霞鹜文楷屏幕阅读版 (#1416)
    * 导入霞鹜文楷屏幕阅读版
    
    * Update custom.css

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,7 +1,8 @@
 // .vitepress/theme/index.js
 
 import DefaultTheme from 'vitepress/theme-without-fonts'; //不使用默认字体
-import 'lxgw-wenkai-screen-web/style.css'; // 导入霞鹜文楷屏幕阅读版
+import 'lxgw-wenkai-screen-web/lxgwwenkaigbscreen/result.css'; // 导入霞鹜文楷屏幕阅读版
+import 'lxgw-wenkai-screen-web/lxgwwenkaimonogbscreen/result.css'; // 导入霞鹜文楷屏幕阅读版
 import 'noto-sans-sc/all.css'; // 导入 noto，思源宋体加粗后辨识度不高
 import './custom.css';
 import Layout from './Layout.vue';
