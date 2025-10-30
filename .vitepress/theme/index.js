67c4b54e0fd363916ef70331c150f54ebb103984
ykla <yklaxds@gmail.com>
2025-10-30 10:42:19 +0800

    Update index.js

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,8 +1,8 @@
 // .vitepress/theme/index.js
 
 import DefaultTheme from 'vitepress/theme-without-fonts'; //不使用默认字体
-import '@wc1font/source-han-serif-sc-vf/font.css'; //导入霞鹜文楷屏幕阅读版
-import 'noto-sans-sc/all.css'; // 导入 noto
+import 'lxgw-wenkai-screen-web/style.css'; // 导入霞鹜文楷屏幕阅读版
+import '@wc1font/source-han-serif-sc-vf/font.css'; // 导入思源宋体
 import './custom.css';
 import Layout from './Layout.vue';
 
