5a9af8b3899520ec92f62fa6c995f13957675de4
ykla <yklaxds@gmail.com>
2025-10-30 10:51:35 +0800

    Update index.js (#1415)
    * Update index.js
    
    * Update custom.css

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -2,7 +2,7 @@
 
 import DefaultTheme from 'vitepress/theme-without-fonts'; //不使用默认字体
 import 'lxgw-wenkai-screen-web/style.css'; // 导入霞鹜文楷屏幕阅读版
-import '@wc1font/source-han-serif-sc-vf/font.css'; // 导入思源宋体
+import 'noto-sans-sc/all.css'; // 导入 noto，思源宋体加粗后辨识度不高
 import './custom.css';
 import Layout from './Layout.vue';
 
